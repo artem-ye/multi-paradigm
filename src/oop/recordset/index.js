61c7c5b307707baa96f37e2aea8611339c490652
artem-ye <shelmafona@gmail.com>
2025-05-09 14:46:56 +0300

    refacotr oop opts

--- src/oop/recordset/index.js
+++ src/oop/recordset/index.js
@@ -3,13 +3,14 @@
 const { console } = require('../../../deps/deps.js');
 const { compose } = require('../../lib/fp.utils.js');
 const { CSV } = require('./CSVParser');
-const { DensityReport } = require('./DensityReport.js');
 const { RecordSet } = require('./RecordSet');
+const { DensityReport } = require('./DensityReport.js');
 
 const createReport = (rawData, opts = {}) => {
   const skipFirst = opts.skipFirst ?? 1;
   const skipLast = opts.skipLast ?? 1;
-  const scheme = opts.scheme || {
+  const outScheme = opts.outScheme || DensityReport.defaultFormat;
+  const inputScheme = opts.inputScheme || {
     city: { colIndex: 0 },
     population: { colIndex: 1, map: parseInt },
     area: { colIndex: 2, map: parseInt },
@@ -18,8 +19,8 @@ const createReport = (rawData, opts = {}) => {
   };
 
   const parse = (data) => CSV.parse(data, { skipFirst, skipLast });
-  const convert = (table) => RecordSet.fromTable(scheme, table);
-  const createReport = (rs) => DensityReport.create(rs).format();
+  const convert = (table) => RecordSet.fromTable(inputScheme, table);
+  const createReport = (rs) => DensityReport.create(rs).format(outScheme);
   const print = (printFn) => (data) => data.map((e) => printFn(e));
 
   const process = compose(print(console.log), createReport, convert, parse);
