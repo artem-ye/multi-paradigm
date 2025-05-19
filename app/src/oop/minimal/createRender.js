54588835bd2cb218d31a385a74f4c94309f80dbd
artem-ye <shelmafona@gmail.com>
2025-05-19 14:11:18 +0300

    oop.minimal compose -> chain

--- app/src/oop/minimal/createRender.js
+++ app/src/oop/minimal/createRender.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { compose } = require('app/dependency/fp.utils.js');
+const { chain } = require('app/dependency/fp.utils.js');
 
 const format = (scheme) => (table) => {
   const meta = Object.entries(scheme);
@@ -10,6 +10,6 @@ const format = (scheme) => (table) => {
 
 const render = (renderFn) => (table) => table.forEach((row) => renderFn(row));
 
-const createRender = (scheme, fn) => compose(render(fn), format(scheme));
+const createRender = (scheme, fn) => chain(format(scheme), render(fn));
 
 module.exports = { createRender };
